1e07736b480259acc4e4726e3e81fcdbd0a7bb14
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-15 21:28:50 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,2 +1,24 @@
 require_relative './part_1_solution.rb'
 
+def apply_coupons(cart, coupons)
+  final = []
+  
+end
+
+def apply_clearance(cart)
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+end
+
+def checkout(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
+end
